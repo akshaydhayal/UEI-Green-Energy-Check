2da8ef12c967fc01c4834f95f09e52ba79758477
akshaydhayal <akshaydhayalkd99@gmail.com>
2024-10-08 19:19:54 +0530

    minor ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -486,9 +486,11 @@ function App() {
 }, [connection, anchorWallet]);
 
   return (
-      <div className="container mx-auto p-4 bg-gray-900 min-h-screen">
-      <h1 className="text-3xl font-bold mb-8 text-center text-white">Green Energy Management</h1>
-      <div className="flex justify-center mb-8">
+    <div className="container mx-auto p-4 bg-gray-900 min-h-screen">
+      <div className="flex justify-between px-4 items-center">
+        <h1 className="text-3xl font-mono tracking-normal font-bold mb-2 flex items-center text-green-500">
+          <Battery className="mr-2" /> UEI Green Energy
+        </h1>
         <WalletMultiButton />
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
